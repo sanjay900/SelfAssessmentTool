50262659a1bb9b9d7f7e1cf7510fe7395cf2dc6c
sanjay900 <sanjay.govind9@gmail.com>
2017-05-22 13:59:28 +1200

    Run no longer needs to exist, we run with junit so you can just provide junit tests that way

--- src/main/java/sat/AbstractTask.java
+++ src/main/java/sat/AbstractTask.java
@@ -5,5 +5,4 @@ import sat.util.TaskDebug;
 public abstract class AbstractTask extends TaskDebug {
     public abstract String getCodeToDisplay();
     public abstract String getMethodsToFill();
-    public abstract void run();
 }
