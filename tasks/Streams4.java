0ea6838ab2a88b5af83258cd5a7193f906ffd047
Kristian Hansen <kris91268@gmail.com>
2017-05-30 10:37:34 +1200

    Add Streams4

---
+++ tasks/Streams4.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import sat.util.*;
+import java.util.stream.*;
+import sat.compiler.annotations.*;
+
+import static org.junit.Assert.*;
+import static sat.util.AssertUtils.*;
+import java.util.*;
+
+/**
+ * Convert an array of integers to a list of strings with the first element starting with 'a', the second with 'b'
+ * and so on using streams. You can assume an integer array will not be longer than 26. So array {4, 5, 6} will
+ * become {"a4", "b5", "c6"} as stored in the returned list.
+ */
+@Task(name="Streams 4: Converting int array to List<String>")
+public abstract class Streams4 {
+
+    @Task
+    public void testConvert1() {
+        List<String> result = Arrays.asList(new String[] {"a1", "b2", "c3", "d4", "e5"});
+        assertTrue(result.equals(convert(new int[] {1, 2, 3, 4, 5})));
+    }
+
+    @Task
+    public void testConvert2() {
+        List<String> result = Arrays.asList(new String[] {"a200", "b102", "c0", "d90"});
+        assertTrue(result.equals(convert(new int[] {200, 102, 0, 90})));
+    }
+
+    public List<String> convert(int[] numbers);
+}
